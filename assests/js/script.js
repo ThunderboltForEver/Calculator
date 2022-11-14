5601012b3e9db9adb6d231fc2f599de0b00f7db5
zana suleiman <zana.suleiman.44@gmail.com>
2022-11-14 17:45:42 +0300

    Added Computer Events

--- assests/js/script.js
+++ assests/js/script.js
@@ -7,11 +7,16 @@ let getScreen = document.querySelector(".screen");
 let getClear = document.querySelectorAll(".clear");
 let number1 = "",
   number2 = "",
+  temporaryNumber = "",
   key = "",
+  temporaryKey = "",
   i = 0,
-  j = 0,
-  k = 0,
-  result = 0;
+  numberOfSections = 0,
+  checkIfDataEntered = 0,
+  result = 0,
+  temporaryResult = 0,
+  anotherOperatorEntered = 0,
+  temporaryNumberTaken = false;
 
 getAllButtons.forEach((item) => {
   item.onclick = () => {
@@ -35,7 +40,7 @@ getButton.forEach((item) => {
     setTimeout(() => {
       item.classList.remove("change-color");
     }, 100);
-    if (j === 0) {
+    if (numberOfSections === 0) {
       if (!e.target.classList.contains("operator")) {
         if (number1.length > 15) {
           alert("Can't put more than 15 digits");
@@ -59,7 +64,7 @@ getButton.forEach((item) => {
           getDiv1.style.color = "red";
           getDiv1.innerHTML = "Error , click a number";
         } else {
-          j++;
+          numberOfSections++;
           getDiv1.innerText += " ";
           key = e.target.value;
 
@@ -68,65 +73,157 @@ getButton.forEach((item) => {
         }
       }
     } else {
+      //Section2
       if (!e.target.classList.contains("operator")) {
         if (number2.length > 15) {
           alert("Can't put more than 15 digits");
         } else {
-          number2 += e.target.value;
-          getDiv1.innerText += e.target.value;
-          k++;
+          if (anotherOperatorEntered === 0) {
+            number2 += e.target.value;
+            getDiv1.innerText += e.target.value;
+            checkIfDataEntered++;
+          } else {
+            checkIfDataEntered++;
+            temporaryNumber = e.target.value;
+            getDiv1.innerText += temporaryNumber;
+            temporaryNumberTaken = true;
+          }
         }
       } else {
-        if (k === 0) {
+        if (checkIfDataEntered === 0) {
           alert("Enter a number");
         } else {
           if (e.target.classList.contains("equal")) {
-          } else {
-            getDiv1.innerText += " ";
-            getDiv1.innerText += e.target.value;
-            getDiv1.innerText += " ";
+            finalCalculation(key, number1, number2);
           }
-          if (e.target.classList.contains("equal")) {
-            switch (key) {
-              case "+":
-                result = parseInt(number1) + parseInt(number2);
-                getDiv2.innerText += result;
-                reset();
-                break;
-              case "-":
-                result = parseInt(number1) - parseInt(number2);
-                getDiv2.innerText += result;
-                reset();
-                break;
-              case "*":
-                result = parseInt(number1) * parseInt(number2);
-                getDiv2.innerText += result;
-                reset();
-                break;
-              case "/":
-                result = parseInt(number1) / parseInt(number2);
-                getDiv2.innerText += result;
-                reset();
-                break;
 
-              default:
-                alert("error");
-                break;
-            }
-          }
-          k = 0;
+          checkIfDataEntered = 0;
         }
       }
     }
   };
 });
+document.addEventListener("keydown", (e) => {
+  getAllButtons.forEach((item) => {
+    if (e.key === item.value || (e.key === "Enter" && item.value === "=")) {
+      if (numberOfSections === 0) {
+        if (!item.classList.contains("operator")) {
+          if (number1.length > 14) {
+            alert("Can't put more than 15 digits");
+          } else {
+            number1 += item.value;
+            if (i === 0) {
+              getDiv1.innerText = "";
+              getDiv2.innerText = "";
+              getDiv1.style.color = "#fff";
+              getDiv1.innerText += item.value;
+              i++;
+            } else {
+              getDiv1.innerText += item.value;
+              i++;
+            }
+          }
+        } else {
+          if (i === 0) {
+            getDiv1.style.color = "red";
+            getDiv1.innerHTML = "Error , click a number";
+            getDiv2.innerText = "";
+          } else if (e.key === "Enter") {
+            alert("Wrong Operator");
+          } else {
+            numberOfSections++;
+            getDiv1.innerText += " ";
+            key = item.value;
+            getDiv1.innerText += key;
+            getDiv1.innerText += " ";
+          }
+        }
+      } else {
+        //Section2
+        if (!item.classList.contains("operator")) {
+          if (number2.length > 14) {
+            alert("Can't put more than 15 digits");
+          } else {
+            number2 += item.value;
+            getDiv1.innerText += item.value;
+            checkIfDataEntered++;
+          }
+        } else {
+          if (checkIfDataEntered === 0) {
+            alert("Enter a number");
+          } else if (item.value === "=" && e.key === "Enter") {
+            finalCalculation(key, number1, number2);
+            checkIfDataEntered = 0;
+          } else {
+            numberOfSections++;
+            getDiv1.innerText += " ";
+            key = item.value;
 
+            getDiv1.innerText += key;
+            getDiv1.innerText += " ";
+            checkIfDataEntered = 0;
+          }
+        }
+      }
+    }
+
+    if (
+      e.key === item.value ||
+      (e.key === "Enter" && item.value === "=") ||
+      (e.key === "Delete" && item.classList.contains("clear"))
+    ) {
+      item.classList.add("change-color");
+      setTimeout(() => {
+        item.classList.remove("change-color");
+      }, 100);
+    }
+    if (e.key === "Delete" && item.classList.contains("clear")) {
+      reset();
+      getDiv1.innerText = "";
+      getDiv2.innerText = "";
+    }
+  });
+});
+
+function finalCalculation(key, number1, number2) {
+  switch (key) {
+    case "+":
+      result = parseInt(number1) + parseInt(number2);
+      getDiv2.innerText += result;
+      reset();
+      break;
+    case "-":
+      result = parseInt(number1) - parseInt(number2);
+      getDiv2.innerText += result;
+      reset();
+      break;
+    case "*":
+      result = parseInt(number1) * parseInt(number2);
+      getDiv2.innerText += result;
+      reset();
+      break;
+    case "/":
+      result = parseInt(number1) / parseInt(number2);
+      getDiv2.innerText += result;
+      reset();
+      break;
+
+    default:
+      alert("error");
+      break;
+  }
+}
 function reset() {
   (number1 = ""),
     (number2 = ""),
     (key = ""),
     (i = 0),
-    (j = 0),
-    (k = 0),
-    (result = 0);
+    (numberOfSections = 0),
+    (checkIfDataEntered = 0),
+    (result = 0),
+    (anotherOperatorEntered = 0),
+    (temporaryKey = ""),
+    (temporaryNumber = ""),
+    (temporaryResult = 0),
+    (temporaryNumberTaken = false);
 }
